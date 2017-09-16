8d8d486ed446e502107cf4a838ecb564544a4886
Bob Busch <rbusch@iqms.com>
2017-09-16 08:32:57 -0400

    Added Client Side Poject

---
+++ DXDashBoardDesigner/DXIQMSDashBoardClientDesigner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DXIQMSDashBoardClientDesigner.MvcApplication" Language="C#" %>
---
+++ DXDashBoardDesigner/DXIQMSDashBoardClientDesigner/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using DevExpress.DashboardWeb.Mvc;
+using DevExpress.DashboardWeb;
+
+namespace DXIQMSDashBoardClientDesigner
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            RouteTable.Routes.MapDashboardRoute("api/dashboard");
+            DashboardConfigurator.Default.SetDashboardStorage(new DashboardFileStorage("~/App_Data/Dashboards"));
+
+        }
+    }
+}
